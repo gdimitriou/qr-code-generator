218e39e0d6771eb2bf5f0674187553006b7f39b2
gdimitriou <georgios.dimitriou@outlook.com>
2021-08-23 12:57:42 +0300

    First commit

---
+++ src/main/java/com/qrcode/generator/QrCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qrcode/generator/QrCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcode.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
